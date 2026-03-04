1317f3b9d0190bfee221a753849818b10feb78ed
TakyDn <renansantiago405@gmail.com>
2026-03-04 04:56:24 -0300

    Update navegacao.js

--- js/navegacao.js
+++ js/navegacao.js
@@ -1,25 +1,13 @@
-/* ===== NAVEGAÇÃO ===== */
-
 function irInicio(){
 
-document.getElementById("telaHerois").classList.add("oculto");
-
-document.getElementById("telaDetalhe").classList.add("oculto");
-
 document.getElementById("telaInicial").classList.remove("oculto");
-
-fecharMenu();
+document.getElementById("telaHerois").classList.add("oculto");
 
 }
 
 function abrirHerois(){
 
 document.getElementById("telaInicial").classList.add("oculto");
-
-document.getElementById("telaDetalhe").classList.add("oculto");
-
 document.getElementById("telaHerois").classList.remove("oculto");
-
-fecharMenu();
 
 }
