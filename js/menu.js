569c7fd5b9d7e49ac3bb5e6753c2eb1083f4c55e
TakyDn <renansantiago405@gmail.com>
2026-03-04 03:30:00 -0300

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,1 +1,21 @@
-.
+/* ===== MENU ===== */
+
+function abrirMenu(){
+
+document.getElementById("menuLateral").classList.add("ativo");
+
+document.getElementById("overlay").classList.add("ativo");
+
+ativarNox();
+
+}
+
+function fecharMenu(){
+
+document.getElementById("menuLateral").classList.remove("ativo");
+
+document.getElementById("overlay").classList.remove("ativo");
+
+document.getElementById("noxContainer").classList.remove("active");
+
+}
