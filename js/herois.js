07c6fa4cfe9f285f6360432ff1d0eccee3916e9c
TakyDn <renansantiago405@gmail.com>
2026-03-04 04:18:16 -0300

    Update herois.js

--- js/herois.js
+++ js/herois.js
@@ -1,39 +1,26 @@
-/* ===== MOSTRAR LUTADOR ===== */
+/* ===== SISTEMA SIMPLES DE HERÓIS ===== */
 
 function mostrarLutador(){
 
 document.getElementById("areaCards").innerHTML = `
-
 <div class="card" onclick="abrirFreya()">
-
 <img src="freya-card.jpg">
-
 <h3>Freya</h3>
-
 </div>
-
 `;
 
 }
 
-
-/* ===== ABRIR FREYA ===== */
-
 function abrirFreya(){
 
 document.getElementById("telaHerois").classList.add("oculto");
-
 document.getElementById("telaDetalhe").classList.remove("oculto");
 
 }
 
-
-/* ===== VOLTAR ===== */
-
 function voltar(){
 
 document.getElementById("telaDetalhe").classList.add("oculto");
-
 document.getElementById("telaHerois").classList.remove("oculto");
 
 }
