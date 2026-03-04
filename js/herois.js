5277fa69d7fa970e14d40bf1d09ce8e32bf11d8c
TakyDn <renansantiago405@gmail.com>
2026-03-04 03:39:31 -0300

    herois.js

---
+++ js/herois.js
@@ -0,0 +1,17 @@
+/* ===== CARDS ===== */
+
+function mostrarLutador(){
+
+document.getElementById("areaCards").innerHTML = `
+
+<div class="card" onclick="abrirFreya()">
+
+<img src="freya-card.jpg">
+
+<h3>Freya</h3>
+
+</div>
+
+`;
+
+}
