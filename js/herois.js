2717f743bb02392b6a7600d7d5b3672c541390c1
TakyDn <renansantiago405@gmail.com>
2026-03-04 06:53:00 -0300

    Update herois.js

--- js/herois.js
+++ js/herois.js
@@ -1,1 +1,20 @@
-VAZIA
+function abrirClasse(classe){
+
+const area = document.getElementById("areaCards");
+
+if(classe === "fighter"){
+
+area.innerHTML = `
+
+<h2>FIGHTERS</h2>
+
+<div class="card" onclick="window.location.href='herois/freya.html'">
+<img src="freya-card.jpg">
+<h3>Freya</h3>
+</div>
+
+`;
+
+}
+
+}
