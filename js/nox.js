5de5df758503d0ae4a9e3e2cb38066793f5d70f5
TakyDn <renansantiago405@gmail.com>
2026-03-04 03:36:30 -0300

    Update nox.js

--- js/nox.js
+++ js/nox.js
@@ -12,3 +12,38 @@ Gosto quando você aprende rápido.`,
 Vitórias começam aqui — não no impulso.`
 
 ];
+
+/* ===== ATIVAR NOX ===== */
+
+function ativarNox(){
+
+const indice = Math.floor(Math.random() * falasMenu.length);
+
+const falaEscolhida = falasMenu[indice];
+
+const box = document.getElementById("noxFala");
+const container = document.getElementById("noxContainer");
+
+container.classList.add("active");
+
+box.innerHTML = "";
+
+let i = 0;
+
+function digitar(){
+
+if(i < falaEscolhida.length){
+
+box.innerHTML += falaEscolhida[i] === "\n" ? "<br>" : falaEscolhida[i];
+
+i++;
+
+setTimeout(digitar,22);
+
+}
+
+}
+
+digitar();
+
+  }
