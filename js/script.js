df5d22c46b66a887a041b24bb1348c5d37f69f05
TakyDn <renansantiago405@gmail.com>
2026-03-06 06:49:40 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -18,3 +18,28 @@ document.addEventListener("contextmenu",function(e){
 e.preventDefault();
 
 });
+
+
+function filtrarHerois(){
+
+let input = document.getElementById("pesquisaHeroi").value.toLowerCase();
+
+let herois = document.querySelectorAll(".heroi-card");
+
+herois.forEach(card => {
+
+let nome = card.innerText.toLowerCase();
+
+if(nome.includes(input)){
+
+card.style.display = "block";
+
+}else{
+
+card.style.display = "none";
+
+}
+
+});
+
+}
