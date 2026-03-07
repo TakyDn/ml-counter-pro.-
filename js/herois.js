bc1139795dce74b22bd10e3abd58582d1751dae3
TakyDn <renansantiago405@gmail.com>
2026-03-06 21:22:34 -0300

    Update herois.js

--- js/herois.js
+++ js/herois.js
@@ -32,3 +32,47 @@ area.innerHTML = `
 }
 
 }
+
+
+function voltarClasses(){
+
+const area = document.getElementById("areaCards");
+
+area.innerHTML = `
+
+<div class="classe-card" onclick="abrirClasse('fighter')">
+<img src="img-classes/fighter.png" class="classe-icon">
+<h3>LUTADOR</h3>
+</div>
+
+<div class="classe-card" onclick="abrirClasse('tank')">
+<img src="img-classes/tank.png" class="classe-icon">
+<h3>TANK</h3>
+</div>
+
+<div class="classe-card" onclick="abrirClasse('assassin')">
+<img src="img-classes/assassin.png" class="classe-icon">
+<h3>ASSASSINO</h3>
+</div>
+
+<div class="classe-card" onclick="abrirClasse('mage')">
+<img src="img-classes/mage.png" class="classe-icon">
+<h3>MAGO</h3>
+</div>
+
+<div class="classe-card" onclick="abrirClasse('marksman')">
+<img src="img-classes/marksman.png" class="classe-icon">
+<h3>ATIRADOR</h3>
+</div>
+
+<div class="classe-card" onclick="abrirClasse('support')">
+<img src="img-classes/support.png" class="classe-icon">
+<h3>SUPORTE</h3>
+</div>
+
+`;
+
+document.getElementById("tituloClasses").style.display = "block";
+document.getElementById("logoTopo").style.display = "block";
+
+}
