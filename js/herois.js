7db53884dc9208d6c3624d3c49dc25d849166df6
TakyDn <renansantiago405@gmail.com>
2026-03-06 21:18:14 -0300

    Update herois.js

--- js/herois.js
+++ js/herois.js
@@ -2,32 +2,33 @@ function abrirClasse(classe){
 
 const area = document.getElementById("areaCards");
 
+document.getElementById("tituloClasses").style.display = "none";
+
+document.getElementById("logoTopo").style.display = "none";
+
 if(classe === "fighter"){
 
 area.innerHTML = `
 
-<h2>FIGHTERS</h2>
+<h2>LUTADORES</h2>
 
-<div class="card" onclick="window.location.href='herois/freya.html'">
+<div class="card">
 <img src="freya-card.jpg">
 <h3>Freya</h3>
+</div>
+
+<div class="card">
+<img src="phoveus-card.jpg">
+<h3>Phoveus</h3>
+</div>
+
+<div class="card">
+<img src="badang-card.jpg">
+<h3>Badang</h3>
 </div>
 
 `;
 
 }
-
-}
-
-
-function abrirClasse(classe){
-
-document.getElementById("tituloClasses").style.display = "none";
-
-document.getElementById("areaCards").style.display = "none";
-
-document.getElementById("telaFighters").classList.remove("oculto");
-
-document.getElementById("logoTopo").style.display = "none";
 
 }
